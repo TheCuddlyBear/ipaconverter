0e42e2e5881c771f52e338360e31984464412e82
TheCuddlyBear <basvthazeveld@outlook.com>
2023-03-05 21:49:43 +0100

    added async functions

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,21 @@ export function IPAFromAlphabet(input: string, comboArray: Array<[string, string
   return IPA;
 }
 
+export function AsyncIPAFromAlphabet(input: string, comboArray: Array<[string, string]>): Promise<string>{
+  return new Promise((resolve, reject) => {
+    var IPA: string = "";
+    var stringArray: Array<string> = input.split("");
+    stringArray.forEach((letter: string) => {
+      comboArray.forEach((combo: [string, string]) => {
+        if (letter == combo[0]) {
+          IPA += combo[1];
+        }
+      });
+    })
+    resolve(IPA);
+  });
+}
+
 export function IPAFromSyllabary(input: string, comboArray: Array<[string, string]>): string{
   var ipa: string = "";
   var stringArray: Array<string> = input.split("");
@@ -22,6 +37,21 @@ export function IPAFromSyllabary(input: string, comboArray: Array<[string, strin
     });
   })
   return ipa;
+}
+
+export function AsyncIPAFromSyllabary(input: string, comboArray: Array<[string, string]>): Promise<string>{
+  return new Promise((resolve, reject) => {
+    var ipa: string = "";
+    var stringArray: Array<string> = input.split("");
+    stringArray.forEach((letter: string) => {
+      comboArray.forEach((combo: [string, string]) => {
+        if (letter == combo[0]) {
+          ipa += combo[1];
+        }
+      });
+    })
+    resolve(ipa);
+  });
 }
 
 export function IPAFromAbugida(input: string, consonantArray: Array<[string, string]>, vowelArray: Array<[string, string]>, placeholder: string): string {
@@ -66,8 +96,55 @@ export function IPAFromAbugida(input: string, consonantArray: Array<[string, str
   return ipa;
 }
 
+export function AsyncIPAFromAbugida(input: string, consonantArray: Array<[string, string]>, vowelArray: Array<[string, string]>, placeholder: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    var ipa: string = "";
+    var comboArray: Array<[string, string]> = [];
+    var regexStr: string = "(";
+    var regex: RegExp;
+    var length: number = consonantArray.length * vowelArray.length + consonantArray.length;
+    var index: number = 0;
+
+    consonantArray.forEach((consonant: [string, string]) => {
+      vowelArray.forEach((vowel: [string, string]) => {
+        var vow: string = vowel[0].replace(new RegExp(placeholder), consonant[0]);
+        comboArray.push([vow, consonant[1] + vowel[1]]);
+      });
+      comboArray.push([consonant[0], consonant[1]]);
+    }
+    );
+
+    comboArray.forEach((combo: [string, string]) => {
+      if(index < length - 1){
+        regexStr += combo[0] + "|";
+        index++;
+      }else{
+        regexStr += combo[0] + ")?";
+      }
+    });
+    regex = new RegExp(regexStr, "gm");
+    var matches = input.match(regex);
+    if(matches != null){
+      var filtered = matches.filter(function (el) {
+        return el != null && el != "";
+      });
+      filtered.forEach((match: string) => {
+        comboArray.forEach((combo: [string, string]) => {
+          if(match == combo[0]){
+            ipa += combo[1];
+          }
+        });
+      });
+    }
+    resolve(ipa);
+  });
+}
+
 export default {
   IPAFromAlphabet,
+  AsyncIPAFromAlphabet,
   IPAFromSyllabary,
-  IPAFromAbugida
+  AsyncIPAFromSyllabary,
+  IPAFromAbugida,
+  AsyncIPAFromAbugida,
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { assert } from 'chai';
 
-import { IPAFromAlphabet, IPAFromSyllabary, IPAFromAbugida } from '../src/index';
+import { IPAFromAlphabet, IPAFromSyllabary, IPAFromAbugida, AsyncIPAFromAbugida, AsyncIPAFromAlphabet, AsyncIPAFromSyllabary } from '../src/index';
 import npmPackage from '../src/index';
 
 describe('NPM Package', () => {
@@ -19,6 +19,18 @@ describe('NPM Package', () => {
 
   it('should have a IPAFromAbugida property', () => {
     assert.property(npmPackage, 'IPAFromAbugida');
+  });
+
+  it('should have a AsyncIPAFromAlphabet property', () => {
+    assert.property(npmPackage, 'AsyncIPAFromAlphabet');
+  });
+
+  it('should have a AsyncIPAFromSyllabary property', () => {
+    assert.property(npmPackage, 'AsyncIPAFromSyllabary');
+  });
+
+  it('should have a AsyncIPAFromAbugida property', () => {
+    assert.property(npmPackage, 'AsyncIPAFromAbugida');
   });
 });
 
@@ -78,6 +90,66 @@ describe('IPAFromAbugida Function', () => {
     const vowelArray: Array<[string, string]> = [["5ा", "a"], ["5ु", "u"]];
     const placeholder = "5"
     const actual = IPAFromAbugida(input, consonantArray, vowelArray, placeholder);
+    assert.equal(actual, expected);
+  });
+});
+
+describe('AsyncIPAFromAlphabet Function', () => {
+  it('should be a function', () => {
+    assert.isFunction(AsyncIPAFromAlphabet);
+  });
+
+  it('should return the correct IPA', async () => {
+    const expected = 'basu';
+    const actual = await AsyncIPAFromAlphabet("βασυ", [["β", "b"], ["α", "a"], ["σ", "s"], ["υ", "u"]]);
+    assert.equal(actual, expected);
+  });
+});
+
+describe('AsyncIPAFromSyllabary Function', () => {
+  it('should be a function', () => {
+    assert.isFunction(AsyncIPAFromSyllabary);
+  });
+
+  it('should return the correct IPA', async () => {
+    const expected = 'ɸɯtatsɯ';
+    const actual = await AsyncIPAFromSyllabary("かざん", [["か", "ɸɯ"], ["ざ", "ta"], ["ん", "tsɯ"], ["υ", "u"]]);
+    assert.equal(actual, expected);
+  });
+});
+
+describe('AsyncIPAFromAbugida Function', () => {
+  it('should be a function', () => {
+    assert.isFunction(AsyncIPAFromAbugida);
+  });
+
+  it('should return the correct IPA', async () => {
+    const expected = 'kase';
+    const input = "กะเสะ"
+    const consonantArray: Array<[string, string]> = [["ก", "k"], ["ส", "s"]];
+    const vowelArray: Array<[string, string]> = [["5ะ", "a"], ["เ5ะ", "e"]];
+    const placeholder = "5"
+    const actual = await AsyncIPAFromAbugida(input, consonantArray, vowelArray, placeholder);
+    assert.equal(actual, expected);
+  });
+
+  it('should return the correct IPA with diacrits', async () => {
+    const expected = 'kuga';
+    const input = "कुगा"
+    const consonantArray: Array<[string, string]> = [["क", "k"], ["ग", "g"]];
+    const vowelArray: Array<[string, string]> = [["5ा", "a"], ["5ु", "u"]];
+    const placeholder = "5"
+    const actual = await AsyncIPAFromAbugida(input, consonantArray, vowelArray, placeholder);
+    assert.equal(actual, expected);
+  });
+
+  it('should return the correct IPA only consonants', async () => {
+    const expected = 'kg';
+    const input = "कग"
+    const consonantArray: Array<[string, string]> = [["क", "k"], ["ग", "g"]];
+    const vowelArray: Array<[string, string]> = [["5ा", "a"], ["5ु", "u"]];
+    const placeholder = "5"
+    const actual = await AsyncIPAFromAbugida(input, consonantArray, vowelArray, placeholder);
     assert.equal(actual, expected);
   });
 });
